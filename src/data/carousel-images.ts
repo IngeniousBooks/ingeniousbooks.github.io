2c7515e9b20468318b7da46940d9ec0dd1c78018
Alex Davis <alexdavis@kkwyg4fwdg.broadband>
2024-02-08 22:19:56 +0000

    feat: implement cycling image carousel controls

---
+++ src/data/carousel-images.ts
@@ -0,0 +1,20 @@
+const carouselImages = [
+  {
+    src: "/src/assets/hero-logo.png",
+    alt: "Ingenious: Elevating the art and craft of publishing, set against a gradient bokke background",
+  },
+  {
+    src: "/src/assets/hero-precision-editing.png",
+    alt: "Text that reads: 'Precision Editing: Superior editorial services that uphold the integrity, clarity, and allure of your content.' To the right are three books, set against a bright gradient and bokke background.",
+  },
+  {
+    src: "/src/assets/hero-stunning-design.png",
+    alt: "Text that reads: 'Stunning Design: From captivating covers to visionary layouts, we bring creative brilliance to every design aspect.' To the right are three books, set against a bright gradient and bokke background.",
+  },
+  {
+    src: "/src/assets/hero-bespoke-project-management.png",
+    alt: "Text that reads: 'Bespoke Project Management: Tailored schedules and streamlined workflows that seamlessly align with the vision and requirements of each publication.' To the right are three books, set against a bright gradient and bokke background.",
+  },
+];
+
+export default carouselImages;
