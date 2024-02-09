29be7fbd9d384099a192af83c33438a860b86a72
Alex Davis <alexdavis@kkwyg4fwdg.broadband>
2024-02-09 08:14:40 +0000

    fix: update attribute keys for React compatibility

--- src/components/dark-left-button.tsx
+++ src/components/dark-left-button.tsx
@@ -20,12 +20,12 @@ const DarkLeftButton = () => (
       </clipPath>
     </defs>
     <g id="c">
-      <circle cx="29" cy="29" r="29" fill="#484848" stroke-width="0" />
+      <circle cx="29" cy="29" r="29" fill="#484848" strokeWidth="0" />
       <g clipPath="url(#d)">
         <path
           d="M20.31,28.26c-.41.41-.41,1.07,0,1.48l11.7,11.7c.41.41,1.08.41,1.48,0,.41-.41.41-1.08,0-1.48l-10.96-10.95,10.96-10.96c.42-.4.44-1.06.04-1.48-.4-.42-1.06-.44-1.48-.04-.01.01-.03.03-.04.04l-11.7,11.7Z"
           fill="#fff"
-          stroke-width="0"
+          strokeWidth="0"
         />
       </g>
     </g>
