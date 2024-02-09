b81aec91b3f90debabac616328293d9fd10f07f0
Alex Davis <alexdavis@kkwyg4fwdg.broadband>
2024-02-09 08:32:35 +0000

    style: create vertical line delineator for header

---
+++ src/components/vertical-line.tsx
@@ -0,0 +1,9 @@
+const VerticalLine = () => (
+  <svg id="b" xmlns="http://www.w3.org/2000/svg" width={2} height={60}>
+    <g id="c">
+      <path d="M.5,0v741" fill="#fff" stroke="#000" stroke-miterlimit="10" />
+    </g>
+  </svg>
+);
+
+export default VerticalLine;
