0238e2c41ae120a742a78663501582332ac24d85
Alex Davis <alexdavis@kkwyg4fwdg.broadband>
2024-02-09 08:42:08 +0000

    style: implement logic to respond to button hover

--- src/components/right-button.tsx
+++ src/components/right-button.tsx
@@ -1,4 +1,14 @@
+import { useState } from "react";
 export default function RightDirection() {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
   return (
     <svg
       id="a"
@@ -6,6 +16,8 @@ export default function RightDirection() {
       xmlnsXlink="http://www.w3.org/1999/xlink"
       height={58}
       width={58}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       <defs>
         <clipPath id="b">
@@ -20,11 +32,17 @@ export default function RightDirection() {
         </clipPath>
       </defs>
       <g opacity=".5">
-        <circle cx="29" cy="29" r="29" fill="#fff" strokeWidth="0" />
+        <circle
+          cx="29"
+          cy="29"
+          r="29"
+          fill={isHovered ? "#484848" : "#fff"}
+          strokeWidth="0"
+        />
         <g clipPath="url(#b)">
           <path
             d="M20.31,28.26c-.41.41-.41,1.07,0,1.48l11.7,11.7c.41.41,1.08.41,1.48,0,.41-.41.41-1.08,0-1.48l-10.96-10.95,10.96-10.96c.42-.4.44-1.06.04-1.48-.4-.42-1.06-.44-1.48-.04-.01.01-.03.03-.04.04l-11.7,11.7Z"
-            fill="#303030"
+            fill={isHovered ? "#fff" : "#303030"}
             strokeWidth="0"
           />
         </g>
