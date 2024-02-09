1f761ff0c88635ec50ea74d6f1b8fa8920296616
Alex Davis <alexdavis@kkwyg4fwdg.broadband>
2024-02-09 08:40:57 +0000

    style: add vertical delineators to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,15 @@
 import IngeniousIcon from "./icon";
 import Logo from "./logo";
 import NavLinks from "./nav-links";
+import VerticalLine from "./vertical-line";
 
 export default function Header() {
   return (
     <header className="header">
       <Logo />
+      <VerticalLine />
       <NavLinks />
+      <VerticalLine />
       <IngeniousIcon />
     </header>
   );
