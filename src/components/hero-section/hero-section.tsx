61d1bbc3c748601e934e7288298c336f6904ac0e
Alex Davis <alexdavis@kkwyg4fwdg.broadband>
2024-02-10 08:17:26 +0000

    feat: implement contact us hero button logic

--- src/components/hero-section/hero-section.tsx
+++ src/components/hero-section/hero-section.tsx
@@ -1,11 +1,14 @@
 import HeroCarousel from "./hero-carousel";
+import { HashLink as Link } from "react-router-hash-link";
 
 export default function HeroSection() {
   return (
     <section className="hero-section">
       <HeroCarousel />
       <h1>Book publishing services</h1>
-      <button>Contact us</button>
+      <Link smooth to="/#contact">
+        <button className="hero-button">Contact us</button>
+      </Link>
     </section>
   );
 }
